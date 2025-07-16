af07724a76df8eb0dc12b2700ab2bf35d170c5cd
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2025-07-17 00:54:26 +0500

    .

--- index.mjs
+++ index.mjs
@@ -2,6 +2,8 @@ import express from 'express';
 import morgan from 'morgan';
 import taskRoutes from './Routes/task.js';
 import users from './Routes/users.js';
+import 'dotenv/config';
+import mongoose from 'mongoose';
 
 const app = express()
 const port = 4000
@@ -9,6 +11,15 @@ const port = 4000
 app.use(morgan('tiny'))
 app.use(express.json())
 app.use("/user" , users)
+
+
+
+console.log("server is running" , process.env.MONGODBURL);
+mongoose.connect(process.env.MONGODBURL)
+    .then(() => console.log('MongoDB connected successfully'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
